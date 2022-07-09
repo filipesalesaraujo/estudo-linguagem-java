81e56a747fadf281d85981e19a7c4abb80419fb8
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-09 16:10:44 -0300

    estrutura condicional

---
+++ src/estrutura_condicional.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class estrutura_condicional {
+
+	public static void main(String[] args) {
+		
+			Scanner sc = new Scanner(System.in);
+			int hora;
+			System.out.print("Digite uma hora do dia: ");
+			hora = sc.nextInt();
+			if (hora < 12) {
+			System.out.println("Bom dia!");
+			}
+			else {
+			System.out.println("Boa tarde!");
+			}
+			sc.close();
+
+	}
+	
+}
